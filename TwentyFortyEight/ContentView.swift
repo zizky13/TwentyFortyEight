82dc642323b62cb935846276f47d7f11326a2884
zizky13 <zikar.nurizky@gmail.com>
2025-07-06 10:28:03 +0700

    Initial Commit

---
+++ TwentyFortyEight/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TwentyFortyEight
+//
+//  Created by Zikar Nurizky on 06/07/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TwentyFortyEight/TwentyFortyEightApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwentyFortyEightApp.swift
+//  TwentyFortyEight
+//
+//  Created by Zikar Nurizky on 06/07/25.
+//
+
+import SwiftUI
+
+@main
+struct TwentyFortyEightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
